57b0b15d5be243a192937ff96ae2dd8add05ecaf
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-04-09 11:54:48 -0600

    Version 0.9

--- src/config.h
+++ src/config.h
@@ -29,7 +29,7 @@ limitations under the License.
 #define SOS_BOARD_SYSTEM_CLOCK 96000000
 #define SOS_BOARD_SYSTEM_MEMORY_SIZE (8192*3)
 #define SOS_BOARD_ID "-L6YKS62TwNuLaRizpA1"
-#define SOS_BOARD_VERSION "0.4"
+#define SOS_BOARD_VERSION "0.9"
 #define SOS_BOARD_NAME "Nucleo-F412ZG"
 
 #define SOS_BOARD_USB_RX_BUFFER_SIZE 512
